be1b633e481e19e001d85eadbd51eb00b7dbbca7
a-henrique <audrinh@gmail.com>
2023-10-20 22:34:52 -0300

    iniciando projeto banco

--- desafio_bancario.py
+++ desafio_bancario.py
@@ -24,37 +24,10 @@ while True:
 
     if opcao == 0:
         print("Depósito")
-        valor_depositado = float(input("Digite o valor a ser depositado: "))
-        if valor_depositado <= 0:
-           print("Valor Inválido")
-           sleep(1)
-        
-        elif valor_depositado > 0:
-            print("Processando...")
-            sleep(1)
-            print(f"Operação encerrada com Sucesso!")
-            saldo += valor_depositado
-            extrato += f"Valor depositado: {valor_depositado:.2f}\n"
-        else:
-            print("digite um valor válido")
 
     elif opcao == 1:
         print("Saque")
-        valor_a_sacar = float(input("Quanto você deseja sacar?"))
-        if numero_saques >= LIMITE_SAQUES:
-            print("Você excedeu o limite de saques diários!!")
-        elif valor_a_sacar > limite:
-            print(f"Você não pode sacar valores acima de R${limite:.2f}")
-        else:
-            if valor_a_sacar > saldo:
-                print("Saldo insuficiente")
-            else:
-                numero_saques += 1
-                saldo -= valor_a_sacar
-                print("Processando...")
-                sleep(1)
-                extrato += f"Valor Saque: -{valor_a_sacar:.2f}!\n"
-                print(f"Operação encerrada com Sucesso!")
+
 
     
     elif opcao == 2:
